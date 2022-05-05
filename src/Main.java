26ae9bb6b2f798dafabfbd3fce4d62399b15f726
Yrii Parhom <parhomenko.yurij@gmail.com>
2022-05-05 20:08:42 +0300

    refactor project

--- src/Main.java
+++ src/Main.java
@@ -3,74 +3,29 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class Main {
+    private static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) {
-        var sb = new StringBuilder();
+        String separator = File.separator;
+        Character pathSeparator = File.pathSeparatorChar;
 
-        var dirSrc = new File("D://Games/src");
-        if (dirSrc.mkdir()) {
-            sb.append("Каталог \"src\" создан \n");
-        }
+        String folderUrlGames = "D:" + separator + separator + "Games" + separator;
+        String folderURLSrc = "D:" + separator + separator + separator + "Games" + separator+ "src" + separator;
+        String folderURLGamesRes = "D:" + pathSeparator + separator +"Games" + separator + "res" +separator;
+        String fileURLGamesSrcMain = "D:" + separator + separator + "Games" + separator + "src" + separator +
+                "main" + separator;
 
-        var dirRes = new File("D://Games/res");
-        if (dirRes.mkdir()) {
-            sb.append("Каталог \"res\" создан \n");
-        }
-
-        var dirSaveGames = new File("D://Games/SaveGames");
-        if (dirSaveGames.mkdir()) {
-            sb.append("Каталог \"SaveGames\" создан \n");
-        }
-
-        var dirTemp = new File("D://Games/temp");
-        if (dirTemp.mkdir()) {
-            sb.append("Каталог \"temp\" создан \n");
-        }
-
-        var dirMain = new File("D://Games/src/main");
-        if (dirMain.mkdir()) {
-            sb.append("Каталог \"main\" создан \n");
-        }
-
-        var dirTest = new File("D://Games/src/test");
-        if (dirTest.mkdir()) {
-            sb.append("Каталог \"test\" создан \n");
-        }
-
-        var dirDrawables = new File("D://Games/res/drawables");
-        if (dirDrawables.mkdir()) {
-            sb.append("Каталог \"drawables\" создан \n");
-        }
-
-        var dirVectors = new File("D://Games/res/vectors");
-        if (dirVectors.mkdir()) {
-            sb.append("Каталог \"vectors\" создан \n");
-        }
-
-        var dirIcons = new File("D://Games/res/icons");
-        if (dirIcons.mkdir()) {
-            sb.append("Каталог \"icons\" создан \n");
-        }
-
-        var fileMainSrcMain = new File("D://Games/src/main//main.java");
-        try {
-            if (fileMainSrcMain.createNewFile()) sb.append("Файл main.java был создан \n");
-        } catch (IOException e) {
-            sb.append("Не удалось создать файл \n");
-        }
-
-        var fileUtilSrcMain = new File("D://Games/src/main//util.java");
-        try {
-            if (fileUtilSrcMain.createNewFile()) sb.append("Файл util.java был создан \n");
-        } catch (IOException e) {
-            sb.append("Не удалось создать файл \n");
-        }
-
-        var fileTempTemp = new File("D://Games/temp//temp.txt");
-        try {
-            if (fileTempTemp.createNewFile()) sb.append("Файл temp.txt был создан \n");
-        } catch (IOException e) {
-            sb.append("Не удалось создать файл \n");
-        }
+        createFolder(folderUrlGames, "src");
+        createFolder(folderUrlGames, "res");
+        createFolder(folderUrlGames, "SaveGames");
+        createFolder(folderUrlGames, "temp");
+        createFolder(folderURLSrc, "main");
+        createFolder(folderURLSrc, "test");
+        createFolder(folderURLGamesRes, "drawable");
+        createFolder(folderURLGamesRes, "vectors");
+        createFolder(folderURLGamesRes, "icons");
+        createFile(fileURLGamesSrcMain, "main.java");
+        createFile(fileURLGamesSrcMain, "util.java");
+        createFile("D:" + separator + separator + "Games" + separator + "temp" + separator, "temp.txt");
 
         try (FileWriter writer = new FileWriter("D://Games/temp//temp.txt", false)) {
             writer.write(String.valueOf(sb));
@@ -79,5 +34,22 @@ public class Main {
         }
 
         System.out.println("Установка завершена");
+    }
+    public static StringBuilder createFolder(String url, String name){
+
+        var dirSrc = new File(url + name);
+        if (dirSrc.mkdir()) {
+            sb.append("Каталог ").append(name).append(" создан \n");
+        }
+        return sb;
+    }
+    public static StringBuilder createFile(String url, String fileName){
+        var fileUtilSrcMain = new File(url, fileName);
+        try {
+            if (fileUtilSrcMain.createNewFile()) sb.append("Файл ").append(fileName).append(" был создан \n");
+        } catch (IOException e) {
+            sb.append("Не удалось создать файл \n");
+        }
+        return sb;
     }
 }
